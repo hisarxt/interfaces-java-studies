3a4ccf41316b713e1b7fdf3cc43d65a73c1cb248
hisarxt <artufa74@gmail.com>
2026-07-22 18:57:37 -0300

    fix:println removed

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,28 +19,28 @@ public class Program {
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
 		System.out.println("Entre com os dados do aluguel");
-		System.out.println("Modelo do carro: ");
+		System.out.print("Modelo do carro: ");
 		String carModel = sc.nextLine();
-		System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+		System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-		System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+		System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
 		
 		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 		
-		System.out.println("Entre com o preco por hora:");
+		System.out.print("Entre com o preco por hora:");
 		double pricePerHour = sc.nextDouble();
-		System.out.println("Entre com o preco por dia:");
+		System.out.print("Entre com o preco por dia:");
 		double pricePerDay = sc.nextDouble();
 		
 		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
 		 
 		rentalService.processInvoice(cr);
 		
-		System.out.println("FATURA:");
-		System.out.println("Pagamento Basico: " + cr.getInvoice().getBasicPayment());
-		System.out.println("Imposto: " + cr.getInvoice().getTax());
-		System.out.println("Pagamento Total: " + cr.getInvoice().getTotalPayment());
+		System.out.print("FATURA:");
+		System.out.print("Pagamento Basico: " + cr.getInvoice().getBasicPayment());
+		System.out.print("Imposto: " + cr.getInvoice().getTax());
+		System.out.print("Pagamento Total: " + cr.getInvoice().getTotalPayment());
 		
 		sc.close(); 
 
